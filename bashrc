f016dc5bfc6daa25025946873e07daef952df50d
Namhoon Kim <nakim@ea.com>
2021-08-13 00:04:23 +0900

    up to date

--- bashrc
+++ bashrc
@@ -134,5 +134,5 @@ export TERM="screen-256color"
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
--- bashrc.d/osx.sh
+++ bashrc.d/osx.sh
@@ -7,4 +7,14 @@ if [[ $(uname) == 'Darwin' ]]; then
     if [ -f $(brew --prefix)/etc/bash_completion ]; then
         . $(brew --prefix)/etc/bash_completion
     fi
+
+    export PATH=/usr/local/mysql/bin:$PATH
+
+    # ncurses setting
+    export PATH="/usr/local/opt/ncurses/bin:$PATH"
+    export LDFLAGS="-L/usr/local/opt/ncurses/lib"
+    export CPPFLAGS="-I/usr/local/opt/ncurses/include"
+    export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig"
+
+    [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
 fi
